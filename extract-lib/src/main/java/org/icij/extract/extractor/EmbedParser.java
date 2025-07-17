2eab9b6ab080fecb997bc653b6d401aad1459be6
Rifat Döver <rifatdover@gmail.com>
2025-07-17 13:24:08 +0100

    chore: Comment out embedded metadata exception keys and bump version to 9.4.6.1

--- extract-lib/src/main/java/org/icij/extract/extractor/EmbedParser.java
+++ extract-lib/src/main/java/org/icij/extract/extractor/EmbedParser.java
@@ -81,13 +81,13 @@ public class EmbedParser extends ParsingEmbeddedDocumentExtractor {
 		} catch (final EncryptedDocumentException e) {
 			logger.error("Unable to decrypt encrypted document embedded in document: \"{}\" ({}) (in \"{}\").",
 					metadata.get(TikaCoreProperties.RESOURCE_NAME_KEY), metadata.get(Metadata.CONTENT_TYPE), root, e);
-			metadata.add(TikaCoreProperties.TIKA_META_EXCEPTION_EMBEDDED_STREAM,
-					ExceptionUtils.getFilteredStackTrace(e));
+//			metadata.add(TikaCoreProperties.TIKA_META_EXCEPTION_EMBEDDED_STREAM,
+//					ExceptionUtils.getFilteredStackTrace(e));
 		} catch (final TikaException e) {
 			logger.error("Unable to parse embedded document: \"{}\" ({}) (in \"{}\").",
 					metadata.get(TikaCoreProperties.RESOURCE_NAME_KEY), metadata.get(Metadata.CONTENT_TYPE), root, e);
-			metadata.add(TikaCoreProperties.TIKA_META_EXCEPTION_EMBEDDED_STREAM,
-					ExceptionUtils.getFilteredStackTrace(e));
+//			metadata.add(TikaCoreProperties.TIKA_META_EXCEPTION_EMBEDDED_STREAM,
+//					ExceptionUtils.getFilteredStackTrace(e));
 		}
 	}
 
